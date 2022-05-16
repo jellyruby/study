d5ea08abf0c6cfdf4db5172b58176972f52b10bc
jellyruby <50604410+jellyruby@users.noreply.github.com>
2022-05-16 17:39:57 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+print("첫번재 단어를 입력해주세요");
+str0 = input();
+print("두번재 단어를 입력해주세요");
+str1 = input();
+print("세번재 단어를 입력해주세요");
+str2 = input();
+
+str = str0[0] + str1[0] + str2[0];
+
+
+print(str);
